eb1c9476ec2e203e7f4341e84acd983acd7b593c
mbareb-leonardo14 <mbareb.le@students.amikom.ac.id>
2022-09-12 10:41:44 +0700

    maintenance

--- application/controllers/Products.php
+++ application/controllers/Products.php
@@ -25,42 +25,6 @@ class Products extends CI_Controller
       );
       $this->load->view('layout/v_wrapper_frontend', $data, FALSE);
    }
-
-   public function category($id_category)
-   {
-      $category = $this->m_home->category($id_category);
-      $data = array(
-         'title'    => $category->category_name,
-         'stuff'    => $this->m_home->get_all_data_stuff($id_category),
-         // 'category' => $this->m_home->get_all_data_cat(),
-         'isi'      => 'v_category_frontend'
-      );
-      $this->load->view('layout/v_wrapper_frontend', $data, FALSE);
-   }
-
-   public function brand($id_brand)
-   {
-      $brand = $this->m_home->brand($id_brand);
-      $data = array(
-         'title'    => $brand->brand_name,
-         'stuff'    => $this->m_home->get_all_data_stuff_brand($id_brand),
-         // 'brand' => $this->m_home->get_all_data_cat(),
-         'isi'      => 'v_brand_frontend'
-      );
-      $this->load->view('layout/v_wrapper_frontend', $data, FALSE);
-   }
-
-   public function detail_product($id_stuff)
-   {
-      $stuff = $this->m_home->detail_product($id_stuff);
-      $data = array(
-         'title'    => $stuff->stuff_name,
-         'stuff'    => $this->m_home->detail_product($id_stuff),
-         'pic'     => $this->m_pic->get_pic($id_stuff),
-         'isi'      => 'v_detail_product'
-      );
-      $this->load->view('layout/v_wrapper_frontend', $data, FALSE);
-   }
 }
 
 /* End of file Home.php */
